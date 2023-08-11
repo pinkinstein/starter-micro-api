07a8f5bdb9b53ee3c33b7cbc7bc413d452f4bf85
pinkinstein <137937435+pinkinstein@users.noreply.github.com>
2023-08-11 15:00:57 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,35 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+const http = require("http")
+const fs = require("fs")
+
+
+
+fs.readFile("./1.html", function(err, html){
+    if (err){
+        throw err   
+    }
+    http.createServer(function(request, response){
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    
+    
+        fetch("http://ip-api.com/json/?fields=61439")
+        .then(resp => resp.json())
+        .then(data => {
+        obj = data;
+       })
+      .then((data) => {
+        var ipaddr = obj.query
+        const myJSON = JSON.stringify(obj);
+
+    
+        fs.appendFileSync("./1.json", myJSON + "\n")
+        fs.writeFileSync("./"+ipaddr + " " + obj.city+".json",myJSON);
+    
+    
+      })
+    }).listen(process.env.PORT || 3000);
+  
+    
+    
+})
